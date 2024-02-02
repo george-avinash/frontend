04f69bfc13d02b7949e5589e72420ad4bc6be3c4
george-avinash <georgeavinash6@gmail.com>
2024-02-02 12:51:42 +0530

    done

--- src/components/HomePage/HomeLayout.js
+++ src/components/HomePage/HomeLayout.js
@@ -1,59 +1,59 @@
 import { Box, Button, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import MovieItem from "./Movies/MovieItem";
 import { Link } from "react-router-dom";
-import { getAllMovies } from "./api-helpers/api-helpers";
+import { getAllMovies } from "../../helpers/api-helpers";
+import CradLayout from "./CradLayout";
 
-const HomePage = () => {
-  const [movies, setMovies] = useState([]);
+const HomeLayout = () => {
+  const [movies, setMovies] = useState();
   useEffect(() => {
     getAllMovies()
-      .then((data) => setMovies(data.movies))
+      .then((data) => setMovies(data))
       .catch((err) => console.log(err));
   }, []);
-  //   console.log(movies);
+  console.log(movies);
   return (
-    <Box width={"100%"} height={"100%"} margin={"auto"} marginTop={2}>
-      <Box margin={"auto"} width={"80%"} height={"40vh"} padding={2}>
+    <Box width="100%" height="100vh" marginTop={2} margin="auto">
+      <Box margin={"auto"} width="80%" height="40%" padding={2} display="flex">
         <img
           src="https://assets-in.bmscdn.com/discovery-catalog/events/et00361924-qbkmjrbczq-landscape.jpg"
           alt="Eagle"
-          width={"100%"}
-          height={"100%"}
+          width="100%"
+          height="100%"
         />
       </Box>
-      <Box padding={5} margin={"auto"}>
+      <Box padding={5} margin="auto">
         <Typography variant="h4" textAlign={"center"}>
-          {" "}
           Latest Releases
         </Typography>
       </Box>
       <Box
-        margin={"auto"}
-        display={"flex"}
-        width={"80%"}
-        justifyContent={"center"}
-        alignItems={"center"}
+        gap={5}
+        margin="auto"
+        width="80%"
         flexWrap={"wrap"}
+        display="flex"
+        justifyContent={"center"}
       >
         {movies &&
           movies
             .slice(0, 4)
             .map((movie, index) => (
-              <MovieItem
-                id={movie.id}
+              <CradLayout
+                id={movie._id}
                 title={movie.title}
-                posterUrl={movie.posterUrl}
                 releaseDate={movie.releaseDate}
+                posterUrl={movie.posterUrl}
+                description={movie.description}
                 key={index}
               />
             ))}
       </Box>
-      <Box display={"flex"} padding={5} margin={"auto"}>
+      <Box display={"flex"} padding={5} margin="auto">
         <Button
+          variant="outlined"
           LinkComponent={Link}
           to="/movies"
-          variant="outlined"
           sx={{ margin: "auto", color: "#2b2d42" }}
         >
           View All Movies
@@ -63,4 +63,4 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+export default HomeLayout;
